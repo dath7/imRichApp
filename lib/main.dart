4d22226ae6492393126686a4262b07dbae4a7b6c
dath7 <hoangdat24112002@gmail.com>
2022-12-19 17:50:42 +0700

    app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text("I AM RICH"),
+        ),
+        body: Center(
+          child: Image.asset('assets/diamond.jpg'),
+        ),
+      ),
+    );
+  }
+}
